d9d6a200d20ced9112ccc29ce7a86e9e9a7fa678
AsilMehaboob <138790394+AsilMehaboob@users.noreply.github.com>
2024-08-02 20:34:57 +0530

    added footer to tweetanalyze page

--- src/app/tweetanalyze/page.tsx
+++ src/app/tweetanalyze/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import TweetAnalyzer from '../components/tweetanalyzer'; // Adjust the path as needed
 import Navbar from '../pages/navbar/navbar';
+import Footer from '../pages/footer/footer';
 
 const TweetAnalyse = () => {
   return (
@@ -11,7 +12,7 @@ const TweetAnalyse = () => {
     <div>
         <TweetAnalyzer />
     </div>
-      
+      <Footer/>
     </div>
   );
 };
